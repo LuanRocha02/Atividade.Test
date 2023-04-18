739985855368d9d4540e707a4a551a729444487d
EyckRocha02 <126567932+LuanRocha02@users.noreply.github.com>
2023-04-18 12:00:25 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print('Olá, bem vindo!')
+nome=str(input('Qual seu nome? '))
+print('É um prazer tê-lô aqui {}'.format(nome.upper()))
